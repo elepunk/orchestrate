c583524935efb454718c24f83b58866e71c5e8df
ahmadshah <ahmadshahhafizan@gmail.com>
2014-01-27 17:18:23 +0800

    add pretty print json for 5.3
    Signed-off-by: ahmadshah <ahmadshahhafizan@gmail.com>

--- src/Elepunk/Orchestrate/PackageCreator.php
+++ src/Elepunk/Orchestrate/PackageCreator.php
@@ -167,7 +167,7 @@ class PackageCreator extends Workbench
 
         $stub['autoload']['psr-0'][$namespace] =  $path;
 
-        $this->files->put($directory.'/../../../composer.json', json_encode($stub, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES));
+        $this->files->put($directory.'/../../../composer.json', $this->encode($stub));
     }
 
     /**
@@ -202,11 +202,6 @@ class PackageCreator extends Workbench
         $this->files->put($directory.'/src/routes.php', $stub);
     }
 
-    protected function getComposerContent($directory)
-    {
-        
-    }
-
     /**
      * [appendAutoloader description]
      * @param  [type] $package   [description]
@@ -216,5 +211,112 @@ class PackageCreator extends Workbench
     protected function appendAutoloader($package, $directory)
     {
         return array("{$package->vendor}\\{$package->name}", 'extension/'.$package->lowerVendor.'/'.$package->lowerName.'/src');
+    }
+
+    /**
+     * Encodes an array into (optionally pretty-printed) JSON
+     *
+     * This code is taken from composer/composer package
+     * https://github.com/composer/composer/blob/master/src/Composer/Json/JsonFile.php
+     * 
+     * Originally licensed under MIT by Nils Adermann, Jordi Boggiano
+     *
+     * @param  mixed  $data    Data to encode into a formatted JSON string
+     * @param  int    $options json_encode options (defaults to JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE)
+     * @return string Encoded json
+     */
+    public static function encode($data, $options = 448)
+    {
+        if (version_compare(PHP_VERSION, '5.4', '>=')) {
+            return json_encode($data, $options);
+        }
+
+        $json = json_encode($data);
+
+        $prettyPrint = (bool) ($options & self::JSON_PRETTY_PRINT);
+        $unescapeUnicode = (bool) ($options & self::JSON_UNESCAPED_UNICODE);
+        $unescapeSlashes = (bool) ($options & self::JSON_UNESCAPED_SLASHES);
+
+        if (!$prettyPrint && !$unescapeUnicode && !$unescapeSlashes) {
+            return $json;
+        }
+
+        $result = '';
+        $pos = 0;
+        $strLen = strlen($json);
+        $indentStr = '    ';
+        $newLine = "\n";
+        $outOfQuotes = true;
+        $buffer = '';
+        $noescape = true;
+
+        for ($i = 0; $i < $strLen; $i++) {
+            // Grab the next character in the string
+            $char = substr($json, $i, 1);
+
+            // Are we inside a quoted string?
+            if ('"' === $char && $noescape) {
+                $outOfQuotes = !$outOfQuotes;
+            }
+
+            if (!$outOfQuotes) {
+                $buffer .= $char;
+                $noescape = '\\' === $char ? !$noescape : true;
+                continue;
+            } elseif ('' !== $buffer) {
+                if ($unescapeSlashes) {
+                    $buffer = str_replace('\\/', '/', $buffer);
+                }
+
+                if ($unescapeUnicode && function_exists('mb_convert_encoding')) {
+                    // http://stackoverflow.com/questions/2934563/how-to-decode-unicode-escape-sequences-like-u00ed-to-proper-utf-8-encoded-cha
+                    $buffer = preg_replace_callback('/\\\\u([0-9a-f]{4})/i', function($match) {
+                        return mb_convert_encoding(pack('H*', $match[1]), 'UTF-8', 'UCS-2BE');
+                    }, $buffer);
+                }
+
+                $result .= $buffer.$char;
+                $buffer = '';
+                continue;
+            }
+
+            if (':' === $char) {
+                // Add a space after the : character
+                $char .= ' ';
+            } elseif (('}' === $char || ']' === $char)) {
+                $pos--;
+                $prevChar = substr($json, $i - 1, 1);
+
+                if ('{' !== $prevChar && '[' !== $prevChar) {
+                    // If this character is the end of an element,
+                    // output a new line and indent the next line
+                    $result .= $newLine;
+                    for ($j = 0; $j < $pos; $j++) {
+                        $result .= $indentStr;
+                    }
+                } else {
+                    // Collapse empty {} and []
+                    $result = rtrim($result)."\n\n".$indentStr;
+                }
+            }
+
+            $result .= $char;
+
+            // If the last character was the beginning of an element,
+            // output a new line and indent the next line
+            if (',' === $char || '{' === $char || '[' === $char) {
+                $result .= $newLine;
+
+                if ('{' === $char || '[' === $char) {
+                    $pos++;
+                }
+
+                for ($j = 0; $j < $pos; $j++) {
+                    $result .= $indentStr;
+                }
+            }
+        }
+
+        return $result;
     }
 }
