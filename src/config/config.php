0c8d6e465a7012d414026cd34089eb75071ac44a
Ahmad Shah Hafizan Hamidin <ahmadshahhafizan@gmail.com>
2014-05-10 05:52:49 +0800

    added config
    Signed-off-by: Ahmad Shah Hafizan Hamidin <ahmadshahhafizan@gmail.com>

---
+++ src/config/config.php
@@ -0,0 +1,16 @@
+<?php
+
+return array(
+
+    /*
+    |--------------------------------------------------------------------------
+    | Orchestra Extension Directory
+    |--------------------------------------------------------------------------
+    |
+    | This is where your extensions will be created
+    |
+    */
+   
+    'extension_directory' => 'extensions'
+
+);
